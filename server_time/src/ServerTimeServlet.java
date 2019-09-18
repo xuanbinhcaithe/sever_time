5bd35ea6f62782719b5247cc8679d538264a544b
= <=gaaraxb123@gmail.com>
2019-09-18 21:35:57 +0700

    server time servlet

---
+++ server_time/src/ServerTimeServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "ServerTimeServlet",urlPatterns = "/index")
+public class ServerTimeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        Date today = new Date();
+        writer.println("<html>");
+        writer.println("<h1> Nguyen Xuan Binh </h1>");
+        writer.println(today );
+        writer.println("</html>");
+
+    }
+}
